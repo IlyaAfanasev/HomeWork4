587dde5f4d778fafc6adc36c20a7a5681512d606
IlyuAfanasev <gecktor.86@mail.ru>
2023-02-20 14:38:43 +0400

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
+    }
+
+    public static void task1() {
+        System.out.println("            Задача 1");
+        int age1 = 23;
+        int age2 = 16;
+        if (age1 >= 18) {
+            System.out.println("Если возраст человека равен " + age1 + ", то он совершеннолетний");
+        }
+        if (age2 < 18) {
+            System.out.println("Если возраст человека равен " + age2 + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
     }
 }
+
